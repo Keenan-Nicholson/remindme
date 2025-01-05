13a92294f30190ca22e0d688228cc72023a7b62c
Keenan Nicholson <keenanbnicholson@gmail.com>
2025-01-05 19:02:59 -0330

    implemented OneTimeJob reminders

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -32,7 +34,7 @@ func runBot() (*discordgo.Session, error) {
 	return discord, nil
 }
 
-func handleCronJob(discord *discordgo.Session) {
+func handleCronJob(discord *discordgo.Session, duration time.Duration, userID string, reminder string) {
 	channel_id := os.Getenv("DISCORD_CHANNEL_ID")
 
 	// create a scheduler
@@ -43,14 +45,14 @@ func handleCronJob(discord *discordgo.Session) {
 
 	// Define the job and task
 	_, err = s.NewJob(
-		gocron.DurationJob(
-			10*time.Second,
+		gocron.OneTimeJob(
+			gocron.OneTimeJobStartDateTime(time.Now().Add(duration)),
 		),
 		gocron.NewTask(
 			func() {
 				// Send a message to a Discord channel
 				channelID := channel_id // Replace with the desired channel ID
-				message := "Hello, World!"
+				message := fmt.Sprintf("Hey <@%s>, this is your reminder to%s!", userID, reminder)
 				_, err := discord.ChannelMessageSend(channelID, message)
 				if err != nil {
 					fmt.Println("Error sending message:", err)
@@ -67,7 +69,7 @@ func handleCronJob(discord *discordgo.Session) {
 	}
 
 	// each job has a unique id
-	fmt.Println("Job ID:", s.Jobs()[0].ID())
+	// fmt.Println("Job ID:", s.Jobs()[0].ID())
 
 	// start the scheduler
 	s.Start()
@@ -85,6 +87,62 @@ func handleCronJob(discord *discordgo.Session) {
 	}
 }
 
+func parseDuration(message string) (time.Duration, error, string) {
+
+	message_contents := strings.Split(message, "remindme!")
+	message = message_contents[1]
+
+	command := strings.Split(message, ":")[0]
+	reminder := strings.Split(message, ":")[1]
+
+	parts := strings.Split(command, " ")
+
+	if len(parts) < 2 {
+		return 0, fmt.Errorf("invalid message format"), ""
+	}
+
+	num, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0, fmt.Errorf("invalid number format"), ""
+	}
+
+	unit := parts[1]
+	var duration time.Duration
+	switch unit {
+	case "days":
+		duration = time.Duration(num) * 24 * time.Hour
+	case "hours":
+		duration = time.Duration(num) * time.Hour
+	case "minutes":
+		duration = time.Duration(num) * time.Minute
+	case "seconds":
+		duration = time.Duration(num) * time.Second
+	default:
+		return 0, fmt.Errorf("invalid time unit"), ""
+	}
+
+	return duration, nil, reminder
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.HasPrefix(m.Content, "remindme!") {
+		duration, err, reminder := parseDuration(m.Content)
+		if err != nil {
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Error: %v", err))
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Reminder set for %v", duration))
+
+		handleCronJob(s, duration, m.Author.ID, reminder)
+
+	}
+}
+
 func main() {
 
 	// Load environment variables
@@ -99,8 +157,7 @@ func main() {
 	}
 	defer discord.Close()
 
-	// Start the cron job to send messages to the server channel
-	handleCronJob(discord)
+	discord.AddHandler(messageCreate)
 
 	// Wait for a signal to stop the bot gracefully
 	sc := make(chan os.Signal, 1)
