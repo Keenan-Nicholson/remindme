b0e79004d3c9ff16444f1a0f5785672ca811ffc3
Keenan Nicholson <keenanbnicholson@gmail.com>
2025-01-18 21:35:44 -0330

    database and cron repop  implemented

--- pkg/bot/populate_cron_scheduler.go
+++ pkg/bot/populate_cron_scheduler.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/Keenan-Nicholson/remindme/pkg/database"
@@ -10,64 +11,66 @@ import (
 )
 
 func PopulateCronScheduleFromDatabase(s *discordgo.Session) error {
-
 	currentTime := time.Now().UTC()
 
 	// Query the database for all reminders
-
 	rows, err := database.GetReminders()
-
 	if err != nil {
 		return fmt.Errorf("failed to query database: %w", err)
 	}
 	defer rows.Close()
 
-	// Iterate over the rows and schedule each reminder
+	// Store all reminders in memory
+	var reminders []struct {
+		ID              int
+		CreatedAt       time.Time
+		Username        string
+		DurationSeconds int
+		Reminder        string
+	}
+
 	for rows.Next() {
-		// channel_id := os.Getenv("DISCORD_CHANNEL_ID")
-		// channelID := channel_id
-
-		var id int
-		var created_at time.Time
-		var username string
-		var durationSeconds int
-		var reminder string
-
-		err := rows.Scan(&id, &created_at, &username, &durationSeconds, &reminder)
-		if err != nil {
-			log.Println("Error scanning row:", err)
+		var reminder struct {
+			ID              int
+			CreatedAt       time.Time
+			Username        string
+			DurationSeconds int
+			Reminder        string
 		}
+		if err := rows.Scan(&reminder.ID, &reminder.CreatedAt, &reminder.Username, &reminder.DurationSeconds, &reminder.Reminder); err != nil {
+			log.Println("Error scanning row:", err)
+			continue
+		}
+		reminders = append(reminders, reminder)
+	}
 
-		if currentTime.Sub(created_at.Add(time.Duration(durationSeconds)*time.Second)) > 0 {
+	// Iterate over stored reminders and process each
+	for _, r := range reminders {
+		reminderTime := r.CreatedAt.Add(time.Duration(r.DurationSeconds) * time.Second)
+
+		if currentTime.Sub(reminderTime) > 0 {
 			// This reminder is in the past, delete it
-			dbErr := database.DeleteReminder(id)
-			if dbErr != nil {
+			if dbErr := database.DeleteReminder(r.ID); dbErr != nil {
 				log.Println("Error deleting reminder from DB:", dbErr)
 			} else {
 				log.Println("Reminder deleted from DB!")
 			}
+		} else if currentTime.Equal(reminderTime) {
+			// Reminder is set to the current time, send it immediately
+			message := fmt.Sprintf("Hey <@%s>, this is your reminder: %s!", r.Username, r.Reminder)
+			log.Printf("Executing reminder for user %s: %s", r.Username, r.Reminder)
+			_, err := s.ChannelMessageSend(os.Getenv("DISCORD_CHANNEL_ID"), message)
+			if err != nil {
+				log.Println("Error sending message:", err)
+			} else {
+				log.Println("Message sent!")
+			}
+		} else {
+			// Reminder is in the future, schedule it
+			durationUntilReminder := reminderTime.Sub(currentTime)
+			log.Printf("Scheduling reminder for user %s: %s in %s", r.Username, r.Reminder, durationUntilReminder)
+			CreateOneTimeCronJob(s, durationUntilReminder, r.Username, r.Reminder, r.ID)
 		}
-
-		// if currentTime.Sub(created_at.Add(time.Duration(durationSeconds)*time.Second)) == 0 {
-		// 	// if somehow the reminder is set to the current time, send it immediately
-		// 	message := fmt.Sprintf("Hey <@%s>, this is your reminder: %s!", username, reminder)
-
-		// 	log.Printf("Executing reminder for user %s: %s", username, reminder)
-		// 	_, err := s.ChannelMessageSend(channelID, message)
-		// 	if err != nil {
-		// 		log.Println("Error sending message:", err)
-		// 	} else {
-		// 		log.Println("Message sent!")
-		// 	}
-		// }
-
-		// if currentTime.Sub(created_at.Add(time.Duration(durationSeconds)*time.Second)) < 0 {
-		// 	// This reminder is in the future, schedule it
-		// 	newDuration := created_at.Add(time.Duration(durationSeconds) * time.Second).Sub(currentTime)
-		// 	log.Printf("Scheduling reminder for user %s: %s in %s", username, reminder, newDuration)
-
-		// 	CreateOneTimeCronJob(s, newDuration, username, reminder, id)
-		// }
 	}
 
 	return nil
