ace9817e16b404d8d787ed22846fcfe359ff102e
Riley Flynn <riley@rileyflynn.me>
2025-03-12 22:35:47 -0230

    Migrate command handling to Switchboard

--- go.mod
+++ go.mod
@@ -7,14 +7,15 @@ require (
 	github.com/go-co-op/gocron/v2 v2.14.1
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.24
+	pkg.nit.so/switchboard v0.0.0-20240930133908-cf045dc997ba
 )
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 )
--- pkg/bot/handlers.go
+++ pkg/bot/handlers.go
@@ -10,108 +10,127 @@ import (
 	"github.com/Keenan-Nicholson/remindme/pkg/utils"
 )
 
-func TimerCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	if i.ApplicationCommandData().Name == "settimer" {
+type timerArgs struct {
+	Duration int            `description:"Duration (int)"`
+	Unit     string         `description:"Unit of time (days / hours / minutes / seconds)"`
+	User     discordgo.User `description:"User"`
+	Reminder string         `description:"Reminder message"`
+}
 
-		duration := i.ApplicationCommandData().Options[0].IntValue()
-		unit := i.ApplicationCommandData().Options[1].StringValue()
-		userID := i.ApplicationCommandData().Options[2].UserValue(s).ID
-		reminder := i.ApplicationCommandData().Options[3].StringValue()
-		channelID := i.ChannelID
-		guildID := i.GuildID
+func TimerCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate, args timerArgs) {
+	duration := args.Duration
+	unit := args.Unit
+	userID := args.User.ID
+	reminder := args.Reminder
+	channelID := i.ChannelID
+	guildID := i.GuildID
 
-		log.Printf("unit: %s, duration: %d, userID: %s, reminder: %s, channel: %s, guild: %s\n", unit, duration, userID, reminder, channelID, guildID)
+	log.Printf("unit: %s, duration: %d, userID: %s, reminder: %s, channel: %s, guild: %s\n", unit, duration, userID, reminder, channelID, guildID)
 
-		var timeDuration time.Duration
-		switch unit {
-		case "days":
-			timeDuration = time.Duration(duration) * 24 * time.Hour
-		case "hours":
-			timeDuration = time.Duration(duration) * time.Hour
-		case "minutes":
-			timeDuration = time.Duration(duration) * time.Minute
-		case "seconds":
-			timeDuration = time.Duration(duration) * time.Second
-		default:
-			return
-		}
-
-		validatedDuration, inputErr := utils.ValidateDuration(timeDuration)
-		if inputErr != nil {
-			log.Println("Error validating duration:", inputErr)
-			return
-		}
-
-		// Insert reminder into the database and handle errors
-		id, err := database.InsertReminder(userID, validatedDuration, reminder, channelID, guildID)
-		if err != nil {
-			log.Println("Error inserting reminder:", err)
-		} else {
-			log.Printf("Reminder created with ID: %d", id)
-		}
-
-		// Handle the cron job
-		go CreateOneTimeCronJob(s, validatedDuration, userID, reminder, id, channelID)
-
-		// Respond to the interaction
-		responseErr := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+	var timeDuration time.Duration
+	switch unit {
+	case "days":
+		timeDuration = time.Duration(duration) * 24 * time.Hour
+	case "hours":
+		timeDuration = time.Duration(duration) * time.Hour
+	case "minutes":
+		timeDuration = time.Duration(duration) * time.Minute
+	case "seconds":
+		timeDuration = time.Duration(duration) * time.Second
+	default:
+		_ = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Content: "Reminder set!",
-				Flags:   1 << 6, // This sets the response to be ephemeral
+				Content: "Invalid unit of time. Please use days, hours, minutes, or seconds.",
+				Flags:   discordgo.MessageFlagsEphemeral,
 			},
 		})
+		return
+	}
 
-		if responseErr != nil {
-			log.Println("Error sending interaction response:", responseErr)
-		}
+	validatedDuration, inputErr := utils.ValidateDuration(timeDuration)
+	if inputErr != nil {
+		log.Println("Error validating duration:", inputErr)
+		return
+	}
+
+	// Insert reminder into the database and handle errors
+	id, err := database.InsertReminder(userID, validatedDuration, reminder, channelID, guildID)
+	if err != nil {
+		log.Println("Error inserting reminder:", err)
+	} else {
+		log.Printf("Reminder created with ID: %d", id)
+	}
+
+	// Handle the cron job
+	go CreateOneTimeCronJob(s, validatedDuration, userID, reminder, id, channelID)
+
+	// Respond to the interaction
+	responseErr := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Reminder set!",
+			Flags:   1 << 6, // This sets the response to be ephemeral
+		},
+	})
+
+	if responseErr != nil {
+		log.Println("Error sending interaction response:", responseErr)
 	}
 }
 
-func DateCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	if i.ApplicationCommandData().Name == "setdate" {
+type dateArgs struct {
+	Year     int            `description:"Year"`
+	Month    int            `description:"Month"`
+	Day      int            `description:"Day"`
+	Hour     int            `description:"Hour (24h)"`
+	Minute   int            `description:"Minute"`
+	User     discordgo.User `description:"User"`
+	Reminder string         `description:"Reminder message"`
+}
 
-		year := int(i.ApplicationCommandData().Options[0].IntValue())
-		month := int(i.ApplicationCommandData().Options[1].IntValue())
-		day := int(i.ApplicationCommandData().Options[2].IntValue())
-		hour := int(i.ApplicationCommandData().Options[3].IntValue())
-		minute := int(i.ApplicationCommandData().Options[4].IntValue())
-		userID := i.ApplicationCommandData().Options[5].UserValue(s).ID
-		reminder := i.ApplicationCommandData().Options[6].StringValue()
-		channelID := i.ChannelID
-		guildID := i.GuildID
+func DateCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate, args dateArgs) {
+	year := args.Year
+	month := args.Month
+	day := args.Day
+	hour := args.Hour
+	minute := args.Minute
+	userID := args.User.ID
+	reminder := args.Reminder
+	channelID := i.ChannelID
+	guildID := i.GuildID
 
-		log.Printf("year: %d, month: %d, day: %d, hour: %d, minute: %d, userID: %s, reminder: %s, channel: %s, guild: %s\n", year, month, day, hour, minute, userID, reminder, channelID, guildID)
+	log.Printf("year: %d, month: %d, day: %d, hour: %d, minute: %d, userID: %s, reminder: %s, channel: %s, guild: %s\n", year, month, day, hour, minute, userID, reminder, channelID, guildID)
 
-		// Handle the cron job
-		timeDuration := utils.ConvertDateToDuration(year, month, day, hour, minute)
+	// Handle the cron job
+	timeDuration := utils.ConvertDateToDuration(year, month, day, hour, minute)
 
-		validatedDuration, inputErr := utils.ValidateDuration(timeDuration)
-		if inputErr != nil {
-			log.Println("Error validating duration:", inputErr)
-			return
-		}
-		// Insert reminder into the database and handle errors
-		id, err := database.InsertReminder(userID, validatedDuration, reminder, channelID, guildID)
-		if err != nil {
-			log.Println("Error inserting reminder:", err)
-		} else {
-			log.Printf("Reminder created with ID: %d", id)
-		}
-
-		go CreateOneTimeCronJob(s, validatedDuration, userID, reminder, id, channelID)
-
-		// Respond to the interaction
-		responseErr := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "Reminder set!",
-				Flags:   1 << 6, // This sets the response to be ephemeral
-			},
-		})
-
-		if responseErr != nil {
-			log.Println("Error sending interaction response:", err)
-		}
+	validatedDuration, inputErr := utils.ValidateDuration(timeDuration)
+	if inputErr != nil {
+		log.Println("Error validating duration:", inputErr)
+		return
 	}
+	// Insert reminder into the database and handle errors
+	id, err := database.InsertReminder(userID, validatedDuration, reminder, channelID, guildID)
+	if err != nil {
+		log.Println("Error inserting reminder:", err)
+	} else {
+		log.Printf("Reminder created with ID: %d", id)
+	}
+
+	go CreateOneTimeCronJob(s, validatedDuration, userID, reminder, id, channelID)
+
+	// Respond to the interaction
+	responseErr := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Reminder set!",
+			Flags:   1 << 6, // This sets the response to be ephemeral
+		},
+	})
+
+	if responseErr != nil {
+		log.Println("Error sending interaction response:", err)
+	}
+
 }
